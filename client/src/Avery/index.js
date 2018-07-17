cec21675e84a38e06f0a06c008bacc4e1430bc41
Dosty Everts <dosty@mustwin.com>
2018-07-16 20:52:21 -0500

    Fixed eslint and made StockTagLabel render as blank if no values present (#14)

--- client/src/Avery/index.js
+++ client/src/Avery/index.js
@@ -1,6 +1,7 @@
 export * from './actions';
 export * from './schema';
 export * from './selectors';
+export { default as BlankLabel } from './components/BlankLabel';
 export { default as Label } from './components/Label';
 export { default as LabelRow } from './components/LabelRow';
 export { default as Sheet } from './components/Sheet';
--- client/src/components/Labels/StockTagLabel.js
+++ client/src/components/Labels/StockTagLabel.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classNames from 'classnames';
+import { BlankLabel } from 'Avery';
 import { toNumber } from 'util/number';
 
 const Row = ({ children, className, style }) => (
@@ -12,10 +13,21 @@ const Col = ({ children, className, style }) => (
         {children}
     </div>
 );
+
 const Label = ({ children, className }) => <label className={classNames('dib fw6 mb0', className)}>{children}</label>;
 
+const RequireStockTagValue = WrappedComponent => {
+    const keys = ['color', 'make', 'miles', 'model', 'receivedFrom', 'receivedOn', 'stockNumber', 'vin', 'year'];
+
+    return label => {
+        const hasValue = keys.find(key => label[key] && (typeof label[key] !== 'string' || label[key].length > 0));
+
+        return hasValue ? <WrappedComponent {...label} /> : <BlankLabel />;
+    };
+};
+
 const StockTagLabel = ({ stockNumber, make, model, vin, color, miles, receivedFrom, receivedOn, year, keyCode, keylessCode }) => (
-    <div className="label-insert" style={{ padding: '0.30em 0.40em' }}>
+    <div style={{ padding: '0.30em 0.40em' }}>
         <Row style={{ fontSize: '0.65em' }}>
             <Col className="w-100">
                 <Label>Stock #</Label> {stockNumber}
@@ -24,7 +36,7 @@ const StockTagLabel = ({ stockNumber, make, model, vin, color, miles, receivedFr
         <Row style={{ fontSize: '0.55em' }}>
             <Col className="w-40">{make}</Col>
             <Col className="w-50">{model}</Col>
-            <Col className="w-10">{year}</Col>
+            <Col className="w-10 tr">{year}</Col>
         </Row>
         <Row style={{ fontSize: '0.50em' }}>
             <Col className="w-100">
@@ -51,9 +63,9 @@ const StockTagLabel = ({ stockNumber, make, model, vin, color, miles, receivedFr
             <Col className="w-60">
                 <Label>From</Label> {receivedFrom}
             </Col>
-            <Col className="w-40">{receivedOn}</Col>
+            <Col className="w-40 tr">{receivedOn}</Col>
         </Row>
     </div>
 );
 
-export default StockTagLabel;
+export default RequireStockTagValue(StockTagLabel);
